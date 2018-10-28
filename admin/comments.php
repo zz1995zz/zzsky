54ecec105f1975d83f8e170d02c8132ebf4af058
zz1995zz <913980996@qq.com>
2018-10-28 17:33:33 +0800

    添加loading效果

--- admin/comments.php
+++ admin/comments.php
@@ -13,6 +13,81 @@ $current_user=zz_get_current_users();
   <link rel="stylesheet" href="/static/assets/vendors/nprogress/nprogress.css">
   <link rel="stylesheet" href="/static/assets/css/admin.css">
   <script src="/static/assets/vendors/nprogress/nprogress.js"></script>
+  <style>
+  .loading {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left:0;
+    right:0;
+    background: rgba(0,0,0,.4);
+    z-index: 999;
+    display: none;
+  }
+  .boxLoading {  
+    width: 50px;
+    height: 50px;
+    margin: auto;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+
+.boxLoading:before {
+  content: '';
+  width: 50px;
+  height: 5px;
+  background: #000;
+  opacity: 0.1;
+  position: absolute;
+  top: 59px;
+  left: 0;
+  border-radius: 50%;
+  animation: box-loading-shadow 0.5s linear infinite;
+}
+.boxLoading:after {
+  content: '';
+  width: 50px;
+  height: 50px;
+  background: skyblue;
+  animation: box-loading-animate 0.5s linear infinite;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: 3px;
+}
+  
+@keyframes box-loading-animate {
+  17% {
+    border-bottom-right-radius: 3px;
+  }
+  25% {
+    transform: translateY(9px) rotate(22.5deg);
+  }
+  50% {
+    transform: translateY(18px) scale(1, .9) rotate(45deg);
+    border-bottom-right-radius: 40px;
+  }
+  75% {
+    transform: translateY(9px) rotate(67.5deg);
+  }
+  100% {
+    transform: translateY(0) rotate(90deg);
+  }
+}
+  
+@keyframes box-loading-shadow {
+  0%, 100% {
+    transform: scale(1, 1);
+  }
+  50% {
+    transform: scale(1.2, 1);
+  }
+}
+  
+  </style>
 </head>
 <body>
   <script>NProgress.start()</script>
@@ -53,7 +128,9 @@ $current_user=zz_get_current_users();
       </table>
     </div>
   </div>
-  
+   <div class="loading">
+     <div class="boxLoading"></div>
+   </div>
   <?php $current_page='comments'; ?>
   <?php include 'inc/aside.php'; ?>
    <!-- 模板引擎 -->
@@ -85,6 +162,17 @@ $current_user=zz_get_current_users();
   <script src="/static/assets/vendors/twbs-pagination/jquery.twbsPagination.js"></script>
  
   <script>
+
+    //loading
+    $(document).ajaxStart(function() {
+      NProgress.start();
+      $('.loading').fadeIn();
+    })
+   .ajaxStop(function() {
+      NProgress.done();
+      $('.loading').fadeOut();
+    });
+    
     function loadPageData(page){
       //ajax发送请求获取数据
       $.getJSON('/admin/api/comments.php', {page:page}, function(res) {
